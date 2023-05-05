18fe51de16c63d698a3380a23b8860976d48ff19
alexay-eth <ah.auyeung@gmail.com>
2023-05-05 16:09:11 +1000

    initial commit

---
+++ app/settings/keys.js
@@ -0,0 +1,22 @@
+/* 
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "firebase/app";
+    import { getAnalytics } from "firebase/analytics";
+    import { FirebaseConfig } from "../settngs/keys";
+
+    // Initialize Firebase
+    const app = initializeApp(FirebaseConfig);
+    const analytics = getAnalytics(app);
+
+*/
+
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBPcYPscHd82Fz-QaI2a5oAr5D3PjXxFQ0",
+    authDomain: "livfieapp.firebaseapp.com",
+    databaseURL: "https://livfieapp.firebaseio.com",
+    projectId: "livfieapp",
+    storageBucket: "livfieapp.appspot.com",
+    messagingSenderId: "432207423329",
+    appId: "1:432207423329:web:b211ba5bd5ee0a62530720",
+    measurementId: "G-4W3WP1LL17"
+  };
