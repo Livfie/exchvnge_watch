3e6a6eed4b9611125842e0e67c4aeb7cc12cae0a
thatalexay <alex@a-y.io>
2023-05-31 07:19:42 +0000

    app store and play store logos in footer

--- app/components/app_footer.tsx
+++ app/components/app_footer.tsx
@@ -1,9 +1,13 @@
 import '../globals.css'
+import Image from 'next/image'
 
 export default function AppFooter() {
 
   return <footer className="exchvnge_footer">
-    Copyright &copy; {new Date().getFullYear()}. <p><a href="https://link.exchvnge.co/watchapp" target="_blank">Download Exchvnge from App Store / Play Store</a>
+    Copyright &copy; {new Date().getFullYear()}. <p><a href="https://link.exchvnge.co/watchapp" target="_blank">
+      <Image src="/appstore-logo.svg" alt="Download on iOS App Store" width="80" height="30"/>
+      <Image src="/playstore-logo.png" alt="Download on Google Play Store" width="80"  height="30"/>
+      </a>
     </p>
 
   </footer>
